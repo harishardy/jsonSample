28474ae81dfcddc4d1c423f8f278af5f0cc988d0
Harish Hardy <mj.harishardy@gmail.com>
2019-05-26 13:37:47 +0530

    image

---
+++ jsonSample/TableViewCellX.swift
@@ -0,0 +1,34 @@
+//
+//  TableViewCellX.swift
+//  jsonSample
+//
+//  Created by Harish Hardy on 26/05/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCellX: UITableViewCell {
+    
+    
+  
+    @IBOutlet weak var imageViewX1: UIImageView!
+    
+    
+    
+    @IBOutlet weak var labelX1: UILabel!
+    
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- jsonSample/ViewController.swift
+++ jsonSample/ViewController.swift
@@ -8,14 +8,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return nameX1.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cellX1 = tableView.dequeueReusableCell(withIdentifier: "cellIdentifierX1", for: indexPath) as! TableViewCellX
+        
+        cellX1.labelX1.text = nameX1[indexPath.row]
+        cellX1.imageViewX1.image = UIImage(named : "contact")
+        
+        return cellX1
+    }
+    
+    
+
+    
+    var nameX1 = [String()]
 
     override func viewDidLoad() {
         
         let pathX = Bundle.main.path(forResource: "employee", ofType: "plist")
         let dictionaryX = NSDictionary(contentsOfFile: pathX!) as! [String: Any]
         
-        if let arrayX1 = (dictionaryX["employees"] as? NSArray)
+        if let arrayX1 = (dictionaryX["employees"] as? [Any])
         {
             for arrayX2 in arrayX1
             {
@@ -26,17 +45,24 @@ class ViewController: UIViewController {
                 print("E-mail ID    : ",detailX1["email"] ?? "not valid option")
                 print("Address      : ",detailX1["address"] ?? "not valid option")
                 
-                    let detailX2a = detailX1["phone"] as? [String : Any]
+                nameX1.append(detailX1["name"] as! String )
                 
-                    print("Phone-Mobile : ",detailX2a?["mobile"] ?? "not valid option")
-                    print("Phone-Home   : ",detailX2a?["home"] ?? "not valid option")
-                    print("Phone-Office : ",detailX2a?["office"] ?? "not valid option")
+                    let detailX2a = detailX1["phone"] as! [String : Any]
+                
+                    print("Phone-Mobile : ",detailX2a["mobile"] ?? "not valid option")
+                    print("Phone-Home   : ",detailX2a["home"] ?? "not valid option")
+                    print("Phone-Office : ",detailX2a["office"] ?? "not valid option")
                 
                 print("Gender       : ",detailX1["gender"] ?? "not valid option")
                 print("")
                 
             }
         }
+        
+        
+        
+        
+       
         
         
         super.viewDidLoad()
